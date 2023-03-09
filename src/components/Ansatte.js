b29889f65f2d7b0c3fd0c3dd2b806a3cedeb3def
annetteaune <annetteaune@outlook.com>
2023-03-09 20:19:32 +0100

    lagt inn placeholder for ansatte

--- src/components/Ansatte.js
+++ src/components/Ansatte.js
@@ -1,3 +1,63 @@
+import Login from "./Login";
+
 export default function Ansatte() {
-	return <span>lage side noe tilsvarende for studenter, men med passende linker etc</span>;
+	return (
+		<>
+			<section className="students-menu">
+				<button>
+					Tjenester <i className="fa-solid fa-chevron-down"></i>
+				</button>
+				<button>
+					Timeplaner <i className="fa-solid fa-chevron-down"></i>
+				</button>
+				<button>
+					Bibliotek <i className="fa-solid fa-chevron-down"></i>
+				</button>
+				<button>
+					Organisering <i className="fa-solid fa-chevron-down"></i>
+				</button>
+			</section>
+			<span className="breadcrumb">
+				Hjem <i className="fa-solid fa-chevron-right"> </i> For ansatte
+			</span>
+			<div className="links-student-container">
+				<section className="shortcuts">
+					<h4>Snarveier</h4>
+					<ul>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe
+							<i className="fa-solid fa-chevron-right"></i>
+						</li>
+					</ul>
+				</section>
+				<section className="profile">
+					<h4>Linker</h4>
+					<ul>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe <i className="fa-solid fa-chevron-right"></i>
+						</li>
+						<li>
+							noe
+							<i className="fa-solid fa-chevron-right"></i>
+						</li>
+					</ul>
+				</section>
+			</div>
+		</>
+	);
 }
